96c07782afff0dbf4c48374d480fc2d08f3acb80
Avo_Mandjian <avomandjian@gmail.com>
2022-03-30 13:58:05 +0300

    ref

--- lib/twa_models.dart
+++ lib/twa_models.dart
@@ -1,4 +1,3 @@
 library twa_models;
 
-export '../models/ecom/collections_models/collections_model.dart';
-export '../models/ecom/products_models/product_model.dart';
+export './models/ecom/collections_models/collections_model.dart';
